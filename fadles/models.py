7d595c2079380a0c372d5c094b87e77a8f3fa693
fadles <fadles@lenta.ru>
2014-06-24 21:10:25 +0000

    settings and models bugs was fixed

--- fadles/models.py
+++ fadles/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from sorl.thumbnail import ImageField
 
+def content_file_name(instance, filename):
+    return '/'.join(['content', filename])
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(max_length=2048)
-    image = ImageField()
+    image = ImageField(upload_to=content_file_name)
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-import os, sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'fadles.settings'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
